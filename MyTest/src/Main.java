392070c15b89b724bbb9197128d945c1235b55ca
KKKlaus <42981813+KKKlaus@users.noreply.github.com>
2023-04-26 12:17:26 -0700

    update

--- MyTest/src/Main.java
+++ MyTest/src/Main.java
@@ -1,12 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int[] num = new int[]{3,5,4};
-        pq.offer(3);
-        pq.offer(5);
-        System.out.println(pq.peek());
+        Main test = new Main();
+        System.out.println(test.wordBreak_2("aaab", Arrays.asList("aa","aaa","a","aab")));
+    }
+
+    public boolean wordBreak_2(String s, List<String> wordDict) {
+        return dfs(s, wordDict, new HashSet<>());
+    }
+
+    // aaaab
+    // a, aa, ab
+    private boolean dfs(String s, List<String> wordDict, HashSet<String> checked) {
+        if (s.isEmpty()) return true;
+        if (checked.contains(s)) return false;
+        checked.add(s);
+
+        for (String word : wordDict) {
+            if (s.startsWith(word) && dfs(s.substring(word.length()), wordDict, checked)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
