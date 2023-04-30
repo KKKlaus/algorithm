93a7ad1283082f6b868de8f24be92a2441ab275b
Zihao Lin <zihaolin@lyft.com>
2023-04-30 10:40:00 -0700

    update

--- MyTest/src/Main.java
+++ MyTest/src/Main.java
@@ -6,25 +6,44 @@ public class Main {
 
     public static void main(String[] args) {
         Main test = new Main();
-        System.out.println(test.wordBreak_2("aaab", Arrays.asList("aa","aaa","a","aab")));
+        int[] start = new int[]{1,1};
+        int[] target = new int[]{4,5};
+        int[][] specialRoads = new int[][]{{1,2,3,3,2},{3,4,4,5,1}};
+        System.out.println(test.minimumCost(start, target, specialRoads));
     }
 
-    public boolean wordBreak_2(String s, List<String> wordDict) {
-        return dfs(s, wordDict, new HashSet<>());
-    }
-
-    // aaaab
-    // a, aa, ab
-    private boolean dfs(String s, List<String> wordDict, HashSet<String> checked) {
-        if (s.isEmpty()) return true;
-        if (checked.contains(s)) return false;
-        checked.add(s);
-
-        for (String word : wordDict) {
-            if (s.startsWith(word) && dfs(s.substring(word.length()), wordDict, checked)) {
-                return true;
+    public int minimumCost(int[] start, int[] target, int[][] specialRoads) {
+        int minCost = getRegularCost(start, target);
+        for (int[] specialRoad : specialRoads) {
+            int[] point1 = new int[]{specialRoad[0], specialRoad[1]};
+            int[] point2 = new int[]{specialRoad[2], specialRoad[3]};
+            int curCost = specialRoad[4];
+            if (minCost <= curCost) continue;
+            if (!isValid(start, target, point1, point2)) continue;
+            if ((isPointTheSame(start, point1) && isPointTheSame(target, point2)) || (isPointTheSame(start, point2) && isPointTheSame(target, point1)) ) {
+                return curCost;
             }
+            if (getRegularCost(point1, point2) < curCost) continue; // may not need this?
+            int cost1 = curCost + minimumCost(start, point1, specialRoads) + minimumCost(point2, target, specialRoads);
+            int cost2 = curCost + minimumCost(start, point2, specialRoads) + minimumCost(point1, target, specialRoads);
+            minCost = Math.min(minCost, Math.min(cost1, cost2));
         }
-        return false;
+        return minCost;
+    }
+
+    private int getRegularCost(int[] p1, int[] p2) {
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+    }
+
+    private boolean isPointTheSame(int[] p1, int[] p2) {
+        return p1[0] == p2[0] && p1[1] == p2[1];
+    }
+
+    private boolean isValid(int[] start, int[] target, int[] p1, int[] p2) {
+        if (!(start[0] <= p1[0] && p1[0] <= target[0])) return false;
+        if (!(start[0] <= p2[0] && p2[0] <= target[0])) return false;
+        if (!(start[1] <= p1[1] && p1[1] <= target[1])) return false;
+        if (!(start[1] <= p2[1] && p2[1] <= target[1])) return false;
+        return true;
     }
 }
